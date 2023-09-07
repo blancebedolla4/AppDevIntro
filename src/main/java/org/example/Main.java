bb4deb842e55bf8f9e73739e076c82778abd4e0c
Alejandra Bedolla <alejandrabedolla@Blancas-MBP.fios-router.home>
2023-09-07 13:43:41 -0400

    reformat code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
+package org.example;
+
 public class Main {
     public static void main(String[] args) {
-
-       String name = "Blanca ";
-       char initial = 'A';
-       String lastName = "Bedolla Lopez";
-
-
+        String name = "Blanca ";
+        char initial = 'A';
+        String lastName = "Bedolla Lopez";
         System.out.println(" My full name is " + name + initial + " " + lastName + "\n");
-
-
-        }
     }
+}
